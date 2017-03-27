464871f72c935bc2c54da8eb709967c3b2f7db44
toxichl <472590061@qq.com>
2017-03-27 22:31:42 +0800

    Simple Update

--- src/website/content/content.vue
+++ src/website/content/content.vue
@@ -1,89 +1,90 @@
 <template>
-    <div class="content-wrapper">
-        <input v-model="ct" type="checkbox" id="ct">
-        <div class="sidenav">
-            <tree :treeData="nav"></tree>
-        </div>
-        <section>
-            <label for="ct">
-                <i class="icon iconfont icon-more"></i>
-            </label>
-            <div class="iu-router-view">
-              <router-view></router-view>
-            </div>
-        </section>
+  <div class="content-wrapper">
+    <input v-model="ct" type="checkbox" id="ct">
+    <div class="sidenav">
+      <tree :treeData="nav"></tree>
     </div>
+    <section>
+      <label for="ct">
+        <i class="icon iconfont icon-more"></i>
+      </label>
+      <div class="iu-router-view">
+        <router-view></router-view>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-    import {NAV_METADATA} from '../router/NAV_METADATA'
-    import tree from '../tree/tree.vue'
-//    import md from '../markdown/md'
-    export default {
-        data() {
-            return {
-                nav: [],
-                ct: true
-            }
-        },
-        methods: {
-            foldSidebar() {
-                this.isSidebarShow = !this.isSidebarShow
-            }
-        },
-        mounted() {
-            this.nav = NAV_METADATA
-        },
-        components: {
-            tree
-        }
+  import {NAV_METADATA} from '../router/NAV_METADATA'
+  import tree from '../tree/tree.vue'
+  //    import md from '../markdown/md'
+  export default {
+    data() {
+      return {
+        nav: [],
+        ct: true
+      }
+    },
+    methods: {
+      foldSidebar() {
+        this.isSidebarShow = !this.isSidebarShow
+      }
+    },
+    mounted() {
+      this.nav = NAV_METADATA
+    },
+    components: {
+      tree
     }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" scope>
-    @import "../../styles/index.styl"
-    @import "../pageConfig.styl"
+  @import "../../styles/index.styl"
+  @import "../pageConfig.styl"
 
-    .content-wrapper
-        #ct
-            display none
-            &:checked ~ section
-                transform translateX($sidebar-width)
-                transform-origin 0% 50%
-            &:checked ~ section label:before
-                content: "收起"
-            &:checked ~ section label i
-                display inline-block
-                transform rotate(180deg)
-        .sidenav
-            background $base-sidebar
-            width $sidebar-width
-            position absolute
-            left 0
-            top $header-height
-            bottom 0
-        section
-            background #fff
-            width 100%
-            position absolute
-            top $header-height
-            bottom 0
-            transition all 0.3s
-            label
-                position absolute
-                left 10px
-                top 10px
-                color #777
-                font-size 12px
-                &:before
-                    content: '展开'
-                i
-                    font-size 12px
-            .iu-router-view
-              padding 40px 20px 0 20px
+  .content-wrapper
+    #ct
+      display none
+      &:checked ~ section
+        transform translateX($sidebar-width)
+        transform-origin 0% 50%
+        width $sidebar-width
+        transition all 0.2s
+      &:checked ~ section label:before
+        content: "收起"
+      &:checked ~ section label i
+        display inline-block
+        transform rotate(180deg)
+    .sidenav
+      background $base-sidebar
+      width $sidebar-width
+      position absolute
+      left 0
+      top $header-height
+      bottom 0
+    section
+      background #fff
+      width 100%
+      position absolute
+      top $header-height
+      bottom 0
+      transition all 0.3s
+      label
+        position absolute
+        left 10px
+        top 10px
+        color #777
+        font-size 12px
+        &:before
+          content: '展开'
+        i
+          font-size 12px
+      .iu-router-view
+        padding 40px 20px 0 20px
 
-    //    ddd
-
+  //    ddd
 
 
 </style>
