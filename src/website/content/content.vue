2842ee816e37175e05a54046506bc3be0d384160
toxichl <472590061@qq.com>
2017-03-27 23:11:33 +0800

    Simple Update

--- src/website/content/content.vue
+++ src/website/content/content.vue
@@ -6,7 +6,7 @@
     </div>
     <section>
       <label for="ct">
-        <i class="icon iconfont icon-more"></i>
+        <i class="icon iconfont icon-back"></i>
       </label>
       <div class="iu-router-view">
         <router-view></router-view>
@@ -45,31 +45,36 @@
   @import "../pageConfig.styl"
 
   .content-wrapper
+    top $header-height
+    position relative
+    display flex
+    width 100%
+    height 100%
     #ct
       width 100%
       display none
       &:checked ~ section
-        transform translateX($sidebar-width)
-        transform-origin 0% 50%
         transition all 0.2s
+      &:checked ~ .sidenav
+        flex 0 0 0
+        width 0
       &:checked ~ section label:before
-        content: "收起"
+        content: "展开"
       &:checked ~ section label i
         display inline-block
         transform rotate(180deg)
     .sidenav
+      transition all 0.5s
       background $base-sidebar
       width $sidebar-width
-      position absolute
-      left 0
-      top $header-height
-      bottom 0
+      height 100%
+      flex 0 0 $sidebar-width
+      position relative
+
     section
       background #fff
-      width 100%
-      position absolute
-      top $header-height
-      bottom 0
+      flex 1
+      position relative
       transition all 0.3s
       label
         position absolute
@@ -78,7 +83,7 @@
         color #777
         font-size 12px
         &:before
-          content: '展开'
+          content: '收起'
         i
           font-size 12px
       .iu-router-view
